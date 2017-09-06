195ffb938c5ec6377749a94442c9447e0bf37548
itsuwari <hakuwu@eis.hokudai.ac.jp>
2017-09-07 03:45:51 +0900

    Fix: load JavaScripts in one file

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 			<div class="container">
 				<div class="pull-left copyright">Copyleft <div class="copyleft">&copy;</div> <?php _e(date('Y'));?>&nbsp;<?php $this->options->title(); ?></div>
 				<ul class="footer-nav pull-right">
-					
+
 
 					<?php if($this->options->miibeian) : ?>
 					<li><a href="http://www.miibeian.gov.cn" rel="nofollow"><?php echo $this->options->miibeian; ?></a></li>
@@ -19,21 +19,22 @@
 			</div>
 		</div>
 	</footer>
-
-	<?php $this->footer(); ?>
-
+    <script src="<?php $this->options->themeUrl('js/bucket.js'); ?></script>
+    <?php /*
 	<script src="<?php $this->options->themeUrl('js/jquery-2.1.4.min.js'); ?>"></script>
 	<script src="<?php $this->options->themeUrl('js/bootstrap.min.js'); ?>"></script>
 	<script src="<?php $this->options->themeUrl('js/material.min.js'); ?>"></script>
 	<script src="<?php $this->options->themeUrl('js/ripples.min.js'); ?>"></script>
 	<script src="<?php $this->options->themeUrl('js/jquery.scrollUp.min.js'); ?>"></script>
-	<script>
+    */
+    ?>
+    <script>
 	$.material.init();
 	$.scrollUp({
 	scrollImg: true,
 	scrollText: "回顶部"
 	});
 	$('#scrollUp').addClass('btn btn-info btn-fab btn-raised mdi-navigation-expand-less');
-	</script>	
+	</script>
 </body>
 </html>
