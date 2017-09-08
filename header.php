0e04b5feb2fae40e7615ff10059f7972d53d54cf
itsuwari <hakuwu@eis.hokudai.ac.jp>
2017-09-08 23:53:35 +0900

    Add: OG meta

--- header.php
+++ header.php
@@ -1,17 +1,22 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <!DOCTYPE HTML>
-<html lang="zh-CN">
+<html lang="en-US">
 <head>
   <link href="https://back.re/icon.jpg" rel="shortcut icon">
   <meta charset="<?php $this->options->charset(); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
   <meta name="renderer" content="webkit">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta property="og:image" content="https://back.re/avatar.png" />
+  <meta content="Hampus blog" property="og:site_name" />
+  <meta content="<?php $this->options->title(); ?>" property="og:title" />
+  <meta content="object" property="og:type" />
+  <meta content="Wellcome to my blog" property="og:description" />
   <title><?php $this->archiveTitle(array(
-            'category'  =>  _t('分类 %s 下的文章'),
+            'category'  =>  _t('Category %s'),
             'search'    =>  _t('包含关键字 %s 的文章'),
-            'tag'       =>  _t('标签 %s 下的文章'),
-            'author'    =>  _t('%s 发布的文章')
+            'tag'       =>  _t('Tag %s'),
+            'author'    =>  _t('Author %s')
          ), '', ' - '); ?><?php $this->options->title(); ?></title>
   <?php if ($this->options->siteIcon): ?>
   <link rel="Shortcut Icon" href="<?php $this->options->siteIcon() ?>" />
